73a06cfa37ba9d357d1cb05fe2fd4650db276e2e
GunaRakulan-GR <gunarakulan@gmail.com>
2021-07-25 19:54:20 +0530

    Create function of ocr

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
 import pytesseract             #Importing ocr framework
 from PIL import Image          #Importing image framework to load image
+
+
+def ocr_core(image_name):
+    text = pytesseract.image_to_string(Image.open(image_name))  
+    return text

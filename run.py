c9e635609d8b2f134bba8685c89019d651f14a0f
GunaRakulan-GR <gunarakulan@gmail.com>
2021-07-25 20:00:37 +0530

    Add a test image

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import pytesseract             #Importing ocr framework
 from PIL import Image          #Importing image framework to load image
 
 
+# This is the main function which convets the image into digital text format.
 def ocr_core(image_name):
     text = pytesseract.image_to_string(Image.open(image_name))  
     return text
+

eb5b8e02ebd4a50d9adf0642259e3ba04e388752
GunaRakulan-GR <gunarakulan@gmail.com>
2021-07-25 20:16:29 +0530

    Intergrate the voice module to the system

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import pytesseract             #Importing ocr framework
 from PIL import Image          #Importing image framework to load image
+import pyttsx3
+engine = pyttsx3.init() # object creation
+
+
+#This is the function for making the computer to talk
+def talk_function(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+voices = engine.getProperty('voices')       #getting details of current voice
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female | 0 for male
 
 
 # This is the main function which convets the image into digital text format.
@@ -9,4 +22,6 @@ def ocr_core(image_name):
 
 
 #Testing the project
-print(ocr_core("test_image_2.png"))
+returnText =  ocr_core("test_image_2.png")
+
+talk_function(returnText)

d3af6ad28cf9c350cac8c8cb297abc87a3c06872
GunaRakulan-GR <gunarakulan@gmail.com>
2022-03-18 15:35:32 +0530

    Add argparse

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+import pyttsx3
+import argparse
 import pytesseract             #Importing ocr framework
 from PIL import Image          #Importing image framework to load image
-import pyttsx3
-engine = pyttsx3.init() # object creation
 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path of the testing image")
+ap.add_argument("-v", "--voice", required=True, help="Path of the testing image")
+args = vars(ap.parse_args())
+
+engine = pyttsx3.init() # object creation
 
 #This is the function for making the computer to talk
 def talk_function(text):
@@ -12,18 +19,23 @@ def talk_function(text):
 
 
 
+
 voices = engine.getProperty('voices')       #getting details of current voice
 engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female | 0 for male
 
 
 # This is the main function which convets the image into digital text format.
 def ocr_core(image_name):
-    text = pytesseract.image_to_string(Image.open(image_name))  
+    text = pytesseract.image_to_string(Image.open(image_name))
     return text
 
 
 #Testing the project
-returnText =  ocr_core("test_image.jpg")
+returnText =  ocr_core(args["image"])
 
-talk_function(returnText)           #testing Done
+if(args["voice"].lower() == "true"):
 
+    talk_function(returnText)
+
+else:
+    print(returnText)
